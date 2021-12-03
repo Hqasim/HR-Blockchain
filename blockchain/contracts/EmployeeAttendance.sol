2bde74465da917b9829269c9606b2ca9f9394eca
hamzahqasim <hqqasim55@gmail.com>
2021-12-03 16:49:00 +0500

    Refined smart contract. Added tests.

--- blockchain/contracts/EmployeeAttendance.sol
+++ blockchain/contracts/EmployeeAttendance.sol
@@ -1,18 +1,67 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+// Smart contracts saves the record of new employee with the following details:
+// Employee ID, First Name, Last Name, Email Address, Department, and Designation.
+// Smart contract allows to the details of employee to be read by supplying
+// Employee ID.
 contract EmployeeAttendance {
-  string employeeName;
+  
+  // Employee struct to save data for each employee
+  struct Employee {
+    uint empID;           // Employee ID
+    string firstName;     // First Name
+    string lastName;      // Last Name
+    string emailAddress;  // Email Address
+    string department;    // Department
+    string designation;   // Designation
+  }
+  
+  // Dynamic array to hold all employee data
+  Employee[] employees;
 
-  constructor() {
-    employeeName = "Hamzah Qasim";
+  // Adds an Employee with its details to employees array
+  function addEmployee(
+    uint empID,
+    string memory firstName,
+    string memory lastName,
+    string memory emailAddress,
+    string memory department,
+    string memory designation
+  ) public {
+    
+    // create new employee struct
+    Employee memory newEmployee = Employee(
+      empID,
+      firstName,
+      lastName,
+      emailAddress,
+      department,
+      designation
+    );
+    
+    // add employee to total list
+    employees.push(newEmployee);
   }
 
-  function setName(string memory _employeeName) public {
-    employeeName = _employeeName;
-  }
-
-  function getName() public view returns (string memory) {
-    return employeeName;
+  // Retrieves employee details by supplying employee id.
+  function getEmployee(uint empID) public view returns (
+    string memory firstName, 
+    string memory lastName,
+    string memory emailAddress,
+    string memory department, 
+    string memory designation) {
+    // iterate over employees array
+    uint i;
+    for (i=0; i<employees.length; i++){
+      Employee memory e = employees[i];
+      // Check if employee with empID exists
+      if (e.empID == empID){
+        // Return the contents of the array
+        return(e.firstName, e.lastName, e.emailAddress, e.department, e.designation);
+      }
+    }
+    // If no match of employee, returns employee not found
+    return("Not Found", "Not Found", "Not Found", "Not Found", "Not Found");
   }
 }
--- blockchain/test/EmployeeAttendanceTest.js
+++ blockchain/test/EmployeeAttendanceTest.js
@@ -1,21 +1,59 @@
+// Import smart contract
 var EmployeeAttendance = artifacts.require("EmployeeAttendance");
 
-contract('EmployeeAttendance', function(accounts){
+// Test instance of imported smart contract
+contract('EmployeeAttendance', () => {
     
-    it("shoud return the name 'Hamzah Qasim'", function(){
-        return EmployeeAttendance.deployed().then(function(instance){
-            return instance.getName.call();
-        }).then(function(name){
-            assert.equal(name, "Hamzah Qasim", "the name was not 'Hamzah Qasim'");
-        });
+    // Checks if a contract is properly deployed and has an address
+    it('Should deploy smart contract properly', async () => {
+        const employeeAttendance = await EmployeeAttendance.deployed();
+        // Assert check if contract address is not empty
+        assert(employeeAttendance.address != "");
     });
-    
-    it("shoud not return the name 'Hamzah Qasim'", function(){
-        return EmployeeAttendance.deployed().then(async function(instance){
-            await instance.setName('ethereum')
-            return instance.getName.call();
-        }).then(function(name){
-            assert.notEqual(name, "Hamzah Qasim", "the name was not 'Hamzah Qasim'");
-        });
+
+    //Add a new employee record. Check if record found
+    it('Should add and get employee', async () => {
+        // Get contract instance
+        const employeeAttendance = await EmployeeAttendance.deployed();
+        // Add employee with sample data
+        await employeeAttendance.addEmployee(
+            1,                      // Employee ID
+            'Hamzah',               // First Name
+            'Qasim',                // Last Name
+            'hqqasim55@gmail.com',  // Email Address
+            'R&D',                  // Department
+            'Developer'             // Designation
+        );
+        // Get employee with the supplied employee id
+        const result = await employeeAttendance.getEmployee(1);
+        // Assert check result with supplied values
+        assert(result[0] === "Hamzah");
+        assert(result[1] === "Qasim");
+        assert(result[2] === "hqqasim55@gmail.com");
+        assert(result[3] === "R&D");
+        assert(result[4] === "Developer");
+    });
+
+    //Add a new employee record. Check if record not found
+    it('Should add and get employee not found', async () => {
+        // Get contract instance
+        const employeeAttendance = await EmployeeAttendance.deployed();
+        // Add employee with sample data
+        await employeeAttendance.addEmployee(
+            1,                      // Employee ID
+            'Hamzah',               // First Name
+            'Qasim',                // Last Name
+            'hqqasim55@gmail.com',  // Email Address
+            'R&D',                  // Department
+            'Developer'             // Designation
+        );
+        // Get employee with the supplied employee id
+        const result = await employeeAttendance.getEmployee(2);
+        // Assert check result with supplied values
+        assert(result[0] != "Hamzah");
+        assert(result[1] != "Qasim");
+        assert(result[2] != "hqqasim55@gmail.com");
+        assert(result[3] != "R&D");
+        assert(result[4] != "Developer");
     });
 });
